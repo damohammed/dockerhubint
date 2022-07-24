a1e8d5e0ec0260e996bf8e4c44be92fa1dcbca09
mukhammadamin <mukhammadamin@localhost.localdomain>
2022-07-24 13:14:34 +0500

    works

---
+++ client/pages/adminpage/index.js
@@ -0,0 +1,7 @@
+export { HomePage } from "./home";
+export { BackendPage } from "./backend";
+export { SettingsPage } from "./settings";
+export { LogPage } from "./logger";
+
+export { SetupPage } from "./setup";
+export { LoginPage } from "./loginpage";
